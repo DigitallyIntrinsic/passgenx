99a9fa9b0d819c018b02f2ed30ee7f445813f40c
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-08-19 14:49:36 -0500

    Added else if statement for 1 positive choice, and coding for converstion to uppercase.

--- script.js
+++ script.js
@@ -33,71 +33,85 @@ function generatePassword() {
   enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
 
   if (!enter) {
-      alert("This needs a value");
+    alert("This needs a value");
   } else if (enter < 8 || enter > 128) {
 
-      enter = parseInt(prompt("You must choose between 8 and 128"));
+    enter = parseInt(prompt("You must choose between 8 and 128"));
 
   } else {
 
-      confirmNumber = confirm("Will this contain numbers?");
-      confirmCharacter = confirm("Will this contain special characters?");
-      confirmUppercase = confirm("Will this contain Uppercase letters?");
-      confirmLowercase = confirm("Will this contain Lowercase letters?");
+    confirmNumber = confirm("Will this contain numbers?");
+    confirmCharacter = confirm("Will this contain special characters?");
+    confirmUppercase = confirm("Will this contain Uppercase letters?");
+    confirmLowercase = confirm("Will this contain Lowercase letters?");
   };
 
   if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
     choices = alert("You must choose a criteria!");
 
-}
+  }
 
-else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+  else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
 
     choices = character.concat(number, alpha, alpha2);
-}
-// This is for 3 positive choices
-else if (confirmCharacter && confirmNumber && confirmUppercase) {
+  }
+  // This is for 3 positive choices
+  else if (confirmCharacter && confirmNumber && confirmUppercase) {
     choices = character.concat(number, alpha2);
-}
-else if (confirmCharacter && confirmNumber && confirmLowercase) {
+  }
+  else if (confirmCharacter && confirmNumber && confirmLowercase) {
     choices = character.concat(number, alpha);
-}
-else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+  }
+  else if (confirmCharacter && confirmLowercase && confirmUppercase) {
     choices = character.concat(alpha, alpha2);
-}
-else if (confirmNumber && confirmLowercase && confirmUppercase) {
+  }
+  else if (confirmNumber && confirmLowercase && confirmUppercase) {
     choices = number.concat(alpha, alpha2);
-}
-    // This is for if they choose 2 items positively and 1 negative choice
-    else if (confirmCharacter && confirmNumber) {
-      choices = character.concat(number);
+  }
+  // This is for if they choose 2 items positively and 1 negative choice
+  else if (confirmCharacter && confirmNumber) {
+    choices = character.concat(number);
 
   } else if (confirmCharacter && confirmLowercase) {
-      choices = character.concat(alpha);
+    choices = character.concat(alpha);
 
   } else if (confirmCharacter && confirmUppercase) {
-      choices = character.concat(alpha2);
+    choices = character.concat(alpha2);
   }
   else if (confirmLowercase && confirmNumber) {
-      choices = alpha.concat(number);
+    choices = alpha.concat(number);
 
   } else if (confirmLowercase && confirmUppercase) {
-      choices = alpha.concat(alpha2);
+    choices = alpha.concat(alpha2);
 
   } else if (confirmNumber && confirmUppercase) {
-      choices = number.concat(alpha2);
+    choices = number.concat(alpha2);
+  }
+  // This is the code for only 1 positive choice
+  else if (confirmCharacter) {
+    choices = character;
+  }
+  else if (confirmNumber) {
+    choices = number;
+  }
+  else if (confirmLowercase) {
+    choices = alpha;
+  }
+  // Created space variable for conversion to uppercase
+  else if (confirmUppercase) {
+    choices = space.concat(alpha2);
+  };
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
   }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 
 
